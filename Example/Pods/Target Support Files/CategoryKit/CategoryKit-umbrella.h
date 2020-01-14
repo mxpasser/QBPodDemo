99ca9068d58f8a74adc4dd52ba7e5cb768623665
qiaojg <qiaojungang@lexue.com>
2020-01-14 10:53:57 +0800

    20200114

---
+++ Example/Pods/Target Support Files/CategoryKit/CategoryKit-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSArray+Extension.h"
+#import "NSArray+json.h"
+#import "NSArray+Log.h"
+#import "NSDate+Extension.h"
+
+FOUNDATION_EXPORT double CategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char CategoryKitVersionString[];
+
